b1b47c9f29974213e0e4a95e8effb4e48c162a27
Chris Malley <cmalley@pixelzoom.com>
2013-02-11 12:39:27 -0700

    add missing copyright and author

--- js/model/property/BooleanProperty.js
+++ js/model/property/BooleanProperty.js
@@ -1,4 +1,10 @@
-/**Subclass of Property that adds methods specific to boolean values*/
+// Copyright 2002-2013, University of Colorado
+
+/**
+ * Subclass of Property that adds methods specific to boolean values
+ *
+ * @author Sam Reid
+ */
 define( ['phetcommon/model/property/Property', 'util/Inheritance'], function ( Property, Inheritance ) {
 
     Inheritance.inheritPrototype( BooleanProperty, Property );
