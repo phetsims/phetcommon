20e0d1100f18c382a98e0fb08c1913d357498eb9
Chris Malley <cmalley@pixelzoom.com>
2013-02-19 14:31:10 -0700

    add optional defaultValue arg to Range constructor

--- js/math/Range.js
+++ js/math/Range.js
@@ -9,14 +9,15 @@ define(
   [],
   function () {
 
-    function Range( min, max ) {
+    function Range( min, max, defaultValue ) {
       this.min = min;
       this.max = max;
+      this.defaultValue = defaultValue || min;
     }
 
     // @return {String}
     Range.prototype.toString = function () {
-      return "[Range (min=" + this.min + " max=" + this.max + ")]";
+      return "[Range (min=" + this.min + " max=" + this.max + " defaultValue=" + this.defaultValue + ")]";
     };
 
     return Range;
