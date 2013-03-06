6441e8774cc5410b1f8e3d33aac7cf2f5f872ed7
Chris Malley <cmalley@pixelzoom.com>
2013-03-06 14:50:42 -0700

    delete phetcommon type (Dimension2D, Vector2D, Point2D), use dot type (Dimension2 and Vector2)

--- js/view/ModelViewTransform2D.js
+++ js/view/ModelViewTransform2D.js
@@ -6,27 +6,27 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 define(
-  [ 'PHETCOMMON/math/Point2D' ],
-  function ( Point2D ) {
+  [ 'DOT/Vector2' ],
+  function ( Vector2 ) {
 
     /**
      * @constructor
      * @param {Number} scale when going from model to view coordinates. 1 unit in the model is this many view units.
-     * @param {Point2D} offset when going from model to view coordinates
+     * @param {Vector2} offset when going from model to view coordinates
      */
     function ModelViewTransform2D( scale, offset ) {
 
       /*
        * Transformation a value from model to view coordinates.
-       * @param {Number|Point2D} value
-       * @return {Number|Point2D}
+       * @param {Number|Vector2} value
+       * @return {Number|Vector2}
        */
       this.modelToView = function ( value ) {
         if ( typeof( value ) === 'number' ) {
           return value * scale;
         }
-        else if ( value instanceof Point2D ) {
-          return new Point2D( ( value.x + offset.x ) * scale, ( value.y + offset.y ) * scale );
+        else if ( value instanceof Vector2 ) {
+          return new Vector2( ( value.x + offset.x ) * scale, ( value.y + offset.y ) * scale );
         }
         else {
           throw new Error( "value has unsupported type: " + typeof( value ) );
@@ -35,15 +35,15 @@ define(
 
       /*
        * Transformation a value from view to model coordinates.
-       * @param {Number|Point2D} value
-       * @return {Number|Point2D}
+       * @param {Number|Vector2} value
+       * @return {Number|Vector2}
        */
       this.viewToModel = function ( value ) {
         if ( typeof( value ) === 'number' ) {
           return value / scale;
         }
-        else if ( value instanceof Point2D ) {
-          return new Point2D( ( value.x / scale ) - offset.x, ( value.y / scale ) - offset.y );
+        else if ( value instanceof Vector2 ) {
+          return new Vector2( ( value.x / scale ) - offset.x, ( value.y / scale ) - offset.y );
         }
         else {
           throw new Error( "value has unsupported type: " + typeof( value ) );
