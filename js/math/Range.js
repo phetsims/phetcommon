d0cd7ce43fefdea672c5162ca9df4aa21d498d6f
Chris Malley <cmalley@pixelzoom.com>
2013-03-07 13:59:39 -0700

    add functionality to Range

--- js/math/Range.js
+++ js/math/Range.js
@@ -15,6 +15,18 @@ define(
       this.defaultValue = defaultValue || min;
     }
 
+    Range.prototype.getLength = function() {
+      return this.max - this.min;
+    };
+
+    Range.prototype.contains = function( value ) {
+      return ( value >= this.min ) && ( value <= this.max );
+    };
+
+    Range.prototype.intersects = function ( range ) {
+      return ( this.max >= range.min ) &&  ( range.max >= this.min );
+    };
+
     // @return {String}
     Range.prototype.toString = function () {
       return "[Range (min=" + this.min + " max=" + this.max + " defaultValue=" + this.defaultValue + ")]";
