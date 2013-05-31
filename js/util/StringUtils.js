f8bb7301c18cc80ef8cfcb242597349ad3f98b9b
Chris Malley <cmalley@pixelzoom.com>
2013-05-31 15:43:09 -0600

    migrate StringUtils to phetcommon.util

---
+++ js/util/StringUtils.js
@@ -0,0 +1,28 @@
+// Copyright 2002-2013, University of Colorado
+
+/**
+ * Collection of utility function related to Strings.
+ */
+define( function() {
+  "use strict";
+
+  function StringUtils() {
+  }
+
+  /**
+   * http://mobzish.blogspot.com/2008/10/simple-messageformat-for-javascript.html
+   * Similar to Java's MessageFormat, supports simple substitution, simple substitution only.
+   * The full MessageFormat specification allows conditional formatting, for example to support pluralisation.
+   * <p>
+   * Eg, StringUtils.format( "{0} + {1}", 2, 3 ) -> "2 + 3"
+   *
+   * @param {String} pattern pattern string, with N placeholders, where N is an integer
+   * @return {String}
+   */
+  StringUtils.format = function( pattern ) {
+    var args = arguments;
+    return pattern.replace( /{(\d)}/g, function( r, n ) { return args[+n + 1];} );
+  };
+
+  return StringUtils;
+} );
