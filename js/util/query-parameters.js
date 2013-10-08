19086b49a25071cb424862b296afccfe65f48d82
Chris Malley <cmalley@pixelzoom.com>
2013-10-08 11:01:09 -0600

    document query params

--- js/util/query-parameters.js
+++ js/util/query-parameters.js
@@ -4,6 +4,19 @@
  * Reads query parameters from browser window's URL.
  * This file must be loaded before requirejs is started up, and this file cannot be loaded as an AMD module.
  * The easiest way to do this is via a <script> tag in your HTML file.
+ * <p>
+ * Available query parameters:
+ *
+ * dev - enable developer-only features
+ * fuzzMouse - randomly sends mouse events to sim
+ * fuzzTouches - randomly sends touch events to sim (not working as of 10/8/13)
+ * locale - test with a specific locale
+ * playbackInputEventLog - plays event logging back from the server, provide an optional name for the session
+ * recordInputEventLog - enables input event logging, provide an optional name for the session, log is available via PhET menu
+ * screenIndex - selects this screen on the home screen
+ * showHomeScreen - if false, go immediate to screenIndex, defaults to screenIndex=0
+ * showPointerArea - touch areas in red, mouse areas in blue, both dotted outlines
+ * standalone - runs screenIndex as a standalone sim, defaults to screenIndex=0
  *
  * @author Sam Reid, PhET
  * @author Chris Malley (PixelZoom, Inc.)
