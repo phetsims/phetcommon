69aa55340544c07cb5bfdd2c8282b1f1e6e55a79
Chris Malley <cmalley@pixelzoom.com>
2014-11-10 18:09:06 -0700

    phetsims/scenery#300 check-assertions.js: fix documentation throughout, fix var names that don't make sense, consolidate redundant tests for 'has'

--- js/util/check-assertions.js
+++ js/util/check-assertions.js
@@ -1,7 +1,9 @@
 // Copyright 2002-2013, University of Colorado Boulder
 
 /**
- * Enables or disables assertions in common libraries based on the 'dev' query parameter flag.
+ * Enables or disables assertions in common libraries using query parameters.
+ * There are two types of assertions: basic and slow. Enabling slow assertions will adversely impact performance.
+ * 'ea' enables basic assertions, 'eall' enables basic and slow assertions.
  * Must be run before RequireJS, and assumes that query-parameters.js has been run.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
@@ -11,29 +13,29 @@
   
   // TODO: separate this logic out into a more common area?
   var isProduction = $( 'meta[name=phet-sim-level]' ).attr( 'content' ) === 'production';
-  
-  var enableAllAssertions = !isProduction && !!window.phetcommon.getQueryParameter( 'eall' ); // for disabling all assertions
-  var enableAssertions = enableAllAssertions || ( !isProduction && !!window.phetcommon.getQueryParameter( 'ea' ) );  // for enabling all assertions
-  
-  var hasBasic = window.has && window.has( 'assert.basic' );
-  var hasSlow = window.has && window.has( 'assert.slow' );
+
+  var enableAllAssertions = !isProduction && !!window.phetcommon.getQueryParameter( 'eall' ); // enables all assertions (basic and slow)
+  var enableBasicAssertions = enableAllAssertions || ( !isProduction && !!window.phetcommon.getQueryParameter( 'ea' ) );  // enables basic assertions
 
   if ( window.has ) {
-    
-    if ( !isProduction && ( hasBasic === undefined && enableAssertions ) || hasBasic ) {
+
+    var hasBasic = window.has( 'assert.basic' );
+    var hasSlow = window.has( 'assert.slow' );
+
+    if ( !isProduction && ( hasBasic === undefined && enableBasicAssertions ) || hasBasic ) {
       console.log( 'enabling basic assertions' );
     }
     
     if ( !isProduction && ( hasSlow === undefined && enableAllAssertions ) || hasSlow ) {
       console.log( 'enabling slow assertions' );
     }
-  
-    // window.assert enabled by default. turned off with 'da'
+
+    // window.assert disabled by default, turned on with 'ea'
     if ( hasBasic === undefined ) {
-      window.has.add( 'assert.basic', function( global, document, anElement ) { return enableAssertions; } );
+      window.has.add( 'assert.basic', function( global, document, anElement ) { return enableBasicAssertions; } );
     }
-    
-    // window.assertSlow disabled by default. turned on with 'ea'
+
+    // window.assertSlow disabled by default, turned on with 'eall'
     if ( hasSlow === undefined ) {
       window.has.add( 'assert.slow', function( global, document, anElement ) { return enableAllAssertions; } );
     }
