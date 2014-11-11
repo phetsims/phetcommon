4c5216fb6d94cadeb6296f68a1a7e8d695a165d2
Sam Reid <reids@colorado.edu>
2014-11-11 10:54:26 -0700

    Added a query parameter to enable the SimIFrameAPI, see https://github.com/phetsims/joist/issues/176

--- js/util/query-parameters.js
+++ js/util/query-parameters.js
@@ -16,6 +16,7 @@
  *
  * accessibility - enable accessibility features, such as keyboard navigation (mileage may vary!)
  * eall - enable all assertions, as above but with more time consuming checks
+ * iframeAPI - enable the SimIFrameAPI, see SimIFrameAPI.js
  * locale - test with a specific locale
  * playbackInputEventLog - plays event logging back from the server, provide an optional name for the session
  * recordInputEventLog - enables input event logging, provide an optional name for the session, log is available via PhET menu
