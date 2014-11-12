87f760f9074d1b70c8e79145020b7801f143f066
Jonathan Olson <olsonsjc@gmail.com>
2014-11-12 00:01:51 -0700

    Changing assertions to the preloaded assert/js/assert.js (which should be included in any HTML file that will include JS running our assertions). see https://github.com/phetsims/scenery/issues/300

--- js/util/check-assertions.js
+++ js/util/check-assertions.js
@@ -10,41 +10,17 @@
  */
 (function() {
   'use strict';
-  
+
   // TODO: separate this logic out into a more common area?
   var isProduction = $( 'meta[name=phet-sim-level]' ).attr( 'content' ) === 'production';
 
   var enableAllAssertions = !isProduction && !!window.phetcommon.getQueryParameter( 'eall' ); // enables all assertions (basic and slow)
   var enableBasicAssertions = enableAllAssertions || ( !isProduction && !!window.phetcommon.getQueryParameter( 'ea' ) );  // enables basic assertions
 
-  if ( window.has ) {
-
-    var hasBasic = window.has( 'assert.basic' );
-    var hasSlow = window.has( 'assert.slow' );
-
-    //TODO last part of this expression looks wrong, hasBasic doesn't mean that it's enabled
-    if ( !isProduction && ( hasBasic === undefined && enableBasicAssertions ) || hasBasic ) {
-      console.log( 'enabling basic assertions' );
-    }
-
-    //TODO last part of this expression looks wrong, hasSlow doesn't mean that it's enabled
-    if ( !isProduction && ( hasSlow === undefined && enableAllAssertions ) || hasSlow ) {
-      console.log( 'enabling slow assertions' );
-    }
-
-    //TODO why not move this above the console output? if expression would simplify to ( !isProduction && enableBasicAssertions )
-    // window.assert.basic undefined by default, turned on with 'ea'
-    if ( hasBasic === undefined ) {
-      window.has.add( 'assert.basic', function( global, document, anElement ) { return enableBasicAssertions; } );
-    }
-
-    //TODO why not move this above the console output? if expression would simplify to ( !isProduction && enableAllAssertions )
-    // window.assert.slow undefined by default, turned on with 'eall'
-    if ( hasSlow === undefined ) {
-      window.has.add( 'assert.slow', function( global, document, anElement ) { return enableAllAssertions; } );
-    }
+  if ( enableBasicAssertions ) {
+    window.assertions.enableAssert();
   }
-  else {
-    console.log( 'has.js not found, using default assertion levels' );
+  if ( enableAllAssertions ) {
+    window.assertions.enableAssertSlow();
   }
 }());
