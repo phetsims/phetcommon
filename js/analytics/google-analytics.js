3cebb2ae12990ea6ca82668d67487a6909500c4d
Jonathan Olson <olsonsjc@gmail.com>
2015-08-27 13:59:55 -0600

    Testing progress for Google Analytics, see https://github.com/phetsims/yotta/issues/12 and https://github.com/phetsims/joist/issues/296

--- js/analytics/google-analytics.js
+++ js/analytics/google-analytics.js
@@ -23,33 +23,12 @@
     return;
   }
 
-  var _gaq = _gaq || [];
-  _gaq.push( [ '_setAccount', 'UA-5033201-1' ] );
-  _gaq.push( [ '_setDomainName', document.domain ] ); // track any domain (except localhost or file://)
-  _gaq.push( [ '_setAllowLinker', true ] ); // allow cross-domain tracking
-  _gaq.push( [ '_setCustomVar', 1, 'Sim Version', ( phet.chipper && phet.chipper.version ), 3 ] ); // slot 1, 3->page-level scope
-  _gaq.push( [ '_setCustomVar', 2, 'Sim Locale', ( phet.chipper && phet.chipper.locale ), 3 ] ); // slot 2, 3->page-level scope
-  _gaq.push( [ '_trackPageview' ] );
-  _gaq.push( [ '_trackPageLoadTime' ] );
+  // Don't track sims that we didn't develop
+  if ( phet.chipper.brand !== 'phet' && phet.chipper.brand !== 'phet-io' ) {
+    return;
+  }
 
-  (function() {
-    var ga = document.createElement( 'script' );
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName( 'script' )[ 0 ];
-    s.parentNode.insertBefore( ga, s );
-  })();
-
-  // Tracking code for Hewlett, see https://github.com/phetsims/joist/issues/109
-  /* Originally below, modified for linter:
-   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-   })(window,document,'script','//www.google-analytics.com/analytics.js','hewlettOERTracker');
-   hewlettOERTracker('create', 'UA-5033010-1', 'phet.colorado.edu');
-   hewlettOERTracker('send', 'pageview');
-   */
+  // Google Analytics snippet for loading the API
   (function( i, s, o, g, r, a, m ) {
     i.GoogleAnalyticsObject = r;
     i[ r ] = i[ r ] || function() {
@@ -59,7 +38,53 @@
     a.async = 1;
     a.src = g;
     m.parentNode.insertBefore( a, m );
-  })( window, document, 'script', ( 'https:' === document.location.protocol ? 'https:' : 'http:' ) + '//www.google-analytics.com/analytics.js', 'hewlettOERTracker' );
-  window.hewlettOERTracker( 'create', 'UA-5033010-1', 'phet.colorado.edu' );
-  window.hewlettOERTracker( 'send', 'pageview' );
+  })( window, document, 'script', ( 'https:' === document.location.protocol ? 'https:' : 'http:' ) + '//www.google-analytics.com/analytics.js', 'googleAnalytics' );
+
+  // Applies custom dimensions that are common for our main and third-party tracker
+  var phetPageviewOptions = {};
+  if ( phet.chipper ) {
+    assert && assert( !phet.chipper.buildTimestamp ||
+                      ( !!phet.chipper.project && !!phet.chipper.version && !!phet.chipper.locale ),
+                      'Missing Google Analytics variable in' );
+
+    if ( phet.chipper.project ) {
+      phetPageviewOptions.dimension1 = phet.chipper.project; // simName custom dimension
+    }
+    if ( phet.chipper.version ) {
+      phetPageviewOptions.dimension2 = phet.chipper.version; // simVersion custom dimension
+    }
+    if ( phet.chipper.locale ) {
+      phetPageviewOptions.dimension3 = phet.chipper.locale; // simLocale custom dimension
+    }
+    if ( phet.chipper.buildTimestamp ) {
+      phetPageviewOptions.dimension4 = phet.chipper.buildTimestamp; // simBuildTimestamp custom dimension
+    }
+  }
+
+  // Main PhET tracker
+  window.googleAnalytics( 'create', {
+    trackingId: 'UA-5033201-1',
+    cookieDomain: 'none'
+  } );
+  window.googleAnalytics( 'send', 'pageview', phetPageviewOptions );
+
+  // Third-party PhET tracker (excludes non-third-party usage, see https://github.com/phetsims/yotta/issues/12)
+  if ( window.location.protocol !== 'file:' &&
+       !document.domain.match( /(.*\.colorado\.edu\.?$)|(^localhost$)|(^127\.0\.0\.1$)/ ) ) {
+    window.googleAnalytics( 'create', {
+      trackingId: 'UA-37615182-2',
+      cookieDomain: 'none',
+      name: 'thirdParty'
+    } );
+    window.googleAnalytics( 'thirdParty.send', 'pageview', phetPageviewOptions );
+  }
+
+  // Hewlett tracker
+  window.googleAnalytics( 'create', {
+    trackingId: 'UA-5033010-1',
+    cookieDomain: 'phet.colorado.edu',
+    name: 'hewlett'
+  } );
+  window.googleAnalytics( 'hewlett.send', 'pageview' );
+
 } )();
