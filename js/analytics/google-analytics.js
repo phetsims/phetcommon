98c7ce9724113836c6f752cc861b16d945fe1902
Jonathan Olson <jonathan.olson@colorado.edu>
2016-12-06 13:06:28 -0700

    Applying changes for Yotta statistics messages, see https://github.com/phetsims/yotta/issues/30

--- js/analytics/google-analytics.js
+++ js/analytics/google-analytics.js
@@ -1,10 +1,8 @@
-// Copyright 2012-2015, University of Colorado Boulder
+// Copyright 2002-2013, University of Colorado Boulder
 
 /**
  * Google analytics collection for HTML5 sims.
  * Code provided by Google and Hewlett, possibly doctored by PhET.
- * Include this script as the last thing in your DOM's head, like this:
- * <script src='common/phetcommon/js/analytics/google-analytics.js'></script>
  *
  * NOTE: Please be careful about modifications, as it relies on external scripts.
  *
@@ -28,6 +26,63 @@
     return;
   }
 
+  var loadType;
+  if ( phet.chipper.getQueryParameter( 'phet-app' ) ) {
+    loadType = 'phet-app';
+  }
+  else if ( top !== self ) {
+    // Checks to see if this sim is embedded - phetsims/chipper#50
+    loadType = 'embedded';
+  }
+  // TODO Add additional conditions for tracking hits from the installer, etc.
+  else {
+    loadType = 'default';
+  }
+
+  // {boolean} - Whether an error was detected with anything relating to google analytics.
+  // See https://github.com/phetsims/yotta/issues/30
+  var googleAnalyticsErrored = false;
+  window.addEventListener( 'error', function( event ) {
+    if ( event &&
+         event.target &&
+         event.target.src &&
+         event.target.src.indexOf &&
+         event.target.src.indexOf( 'google-analytics' ) >= 0 ) {
+      googleAnalyticsErrored = true;
+    }
+  }, true );
+
+  // {boolean} - Whether analytics.js successfully loaded, see https://github.com/phetsims/yotta/issues/30
+  var googleAnalyticsLoaded = false;
+  function onGoogleAnalyticsLoad() {
+    googleAnalyticsLoaded = true;
+  }
+
+  var pingParams = 'pingver=2&' +
+                   'project=' + encodeURIComponent( phet.chipper.project ) + '&' +
+                   'version=' + encodeURIComponent( phet.chipper.version ) + '&' +
+                   'locale=' + encodeURIComponent( phet.chipper.locale ) + '&' +
+                   'buildTimestamp=' + encodeURIComponent( phet.chipper.buildTimestamp ) + '&' +
+                   'domain=' + encodeURIComponent( document.domain ) + '&' +
+                   'href=' + encodeURIComponent( window.location.href ) + '&' +
+                   'type=html&' +
+                   'timestamp=' + encodeURIComponent( Date.now() ) + '&' +
+                   'loadType=' + encodeURIComponent( loadType ) + '&' +
+                   'ref=' + encodeURIComponent( document.referrer );
+
+  function pingURL( url ) {
+    var img = document.createElement( 'img' );
+    img.src = url;
+  }
+
+  pingURL( 'https://phet.colorado.edu/yotta/immediate.gif?' + pingParams );
+
+  window.addEventListener( 'load', function( event ) {
+    pingURL( 'https://phet.colorado.edu/yotta/sanity.gif?' + pingParams + '&' +
+             'gaError=' + encodeURIComponent( googleAnalyticsErrored ) + '&' +
+             'gaLoaded=' + encodeURIComponent( googleAnalyticsLoaded ) );
+  }, false );
+
   // Google Analytics snippet for loading the API
   (function( i, s, o, g, r, a, m ) {
     i.GoogleAnalyticsObject = r;
@@ -40,12 +95,12 @@
     m.parentNode.insertBefore( a, m );
   })( window, document, 'script', ( 'https:' === document.location.protocol ? 'https:' : 'http:' ) + '//www.google-analytics.com/analytics.js', 'googleAnalytics' );
 
-  // Applies custom dimensions that are common for our main and third-party tracker
+  // Applies custom dimensions that are common for our main, third-party, and phet-io tracker
   var phetPageviewOptions = {};
   if ( phet.chipper ) {
     assert && assert( !phet.chipper.buildTimestamp ||
                       ( !!phet.chipper.project && !!phet.chipper.version && !!phet.chipper.locale ),
-                      'Missing Google Analytics variable in' );
+      'Missing Google Analytics variable in' );
 
     if ( phet.chipper.project ) {
       phetPageviewOptions.dimension1 = phet.chipper.project; // simName custom dimension
@@ -59,9 +114,15 @@
     if ( phet.chipper.buildTimestamp ) {
       phetPageviewOptions.dimension4 = phet.chipper.buildTimestamp; // simBuildTimestamp custom dimension
     }
+    phetPageviewOptions.dimension5 = loadType;
+    phetPageviewOptions.dimension6 = document.referrer;
   }
 
   var offlineSimLocation = 'offline/html/' + phet.chipper.project + '_' + phet.chipper.locale;
+
+  // Put our function in the queue, to be invoked when the analytics.js has fully loaded.
+  // See https://github.com/phetsims/yotta/issues/30
+  window.googleAnalytics( onGoogleAnalyticsLoad );
 
   // Main PhET tracker
   window.googleAnalytics( 'create', {
