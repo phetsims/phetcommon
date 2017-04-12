099fe5818b259db1139cbc242f68ef52b547c4e4
Chris Malley <cmalley@pixelzoom.com>
2017-04-12 13:01:15 -0600

    fix config.js for unit tests, https://github.com/phetsims/phetcommon/issues/39

--- js/config.js
+++ js/config.js
@@ -1,13 +1,25 @@
-// Copyright 2013-2015, University of Colorado Boulder
+// Copyright 2013-2017, University of Colorado Boulder
 
 require.config( {
   deps: [ 'main' ],
 
   paths: {
-    PHET_CORE: '../../phet-core/js',
-    KITE: '../../kite/js',
-    DOT: '../../dot/js',
+
+    // third-party libs
+    text: '../../sherpa/lib/text-2.0.12',
+
+    // PhET plugins
+    ifphetio: '../../chipper/js/requirejs-plugins/ifphetio',
+
+    // PhET libs, uppercase names to identify them in require.js imports
     AXON: '../../axon/js',
+    DOT: '../../dot/js',
+    KITE: '../../kite/js',
+    PHET_CORE: '../../phet-core/js',
+    REPOSITORY: '..',
+    TANDEM: '../../tandem/js',
+
+    // this repository
     PHETCOMMON: '.'
   },
 
