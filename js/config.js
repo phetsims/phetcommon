7ec5d29971d7faf69f31e028c45b17df040b721b
zepumph <michael.kauzmann@colorado.edu>
2017-06-16 12:12:16 -0800

    remove BRAND from config files, https://github.com/phetsims/phet-io/issues/1106 https://github.com/phetsims/scenery/issues/626

--- js/config.js
+++ js/config.js
@@ -19,10 +19,6 @@ require.config( {
     REPOSITORY: '..',
     TANDEM: '../../tandem/js',
 
-    // Needs to be compatible in the sim, and also with scenery unit tests.
-    BRAND: '../../brand/' + (window.phet && phet.chipper && phet.chipper.brand ? phet.chipper.brand : 'adapted-from-phet') + '/js',
-
-
     // this repository
     PHETCOMMON: '.'
   },
