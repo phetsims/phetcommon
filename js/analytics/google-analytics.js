f2674fa6389a48d3553d4805ce1490b1ff2c04c8
Jonathan Olson <jonathan.olson@colorado.edu>
2019-07-25 01:47:50 -0600

    Adding storage:'none' to Google Analytics, see https://github.com/phetsims/phet-ios-app/issues/512

--- js/analytics/google-analytics.js
+++ js/analytics/google-analytics.js
@@ -134,6 +134,7 @@
     // Main PhET tracker
     window.googleAnalytics( 'create', {
       trackingId: 'UA-5033201-1',
+      storage: 'none',
       cookieDomain: 'none'
     } );
     if ( window.location.protocol === 'file:' ) {
@@ -148,6 +149,7 @@
     if ( phet.chipper.brand === 'phet-io' ) {
       window.googleAnalytics( 'create', {
         trackingId: 'UA-37615182-3',
+        storage: 'none',
         cookieDomain: 'none',
         name: 'io'
       } );
@@ -165,6 +167,7 @@
          !document.domain.match( /(.*\.colorado\.edu\.?$)|(^localhost$)|(^127\.0\.0\.1$)/ ) ) {
       window.googleAnalytics( 'create', {
         trackingId: 'UA-37615182-2',
+        storage: 'none',
         cookieDomain: 'none',
         name: 'thirdParty'
       } );
@@ -175,6 +178,7 @@
     // Hewlett tracker
     window.googleAnalytics( 'create', {
       trackingId: 'UA-5033010-35',
+      storage: 'none',
       cookieDomain: 'phet.colorado.edu',
       name: 'hewlett'
     } );
@@ -185,6 +189,7 @@
     if ( phet.chipper.queryParameters.ga ) {
       window.googleAnalytics( 'create', {
         trackingId: phet.chipper.queryParameters.ga,
+        storage: 'none',
         cookieDomain: 'none', // don't require the tracking from our site
         name: 'external'
       } );
