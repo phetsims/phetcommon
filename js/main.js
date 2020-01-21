79c5e2ee449046e68a6e5d6fe4a6e9bb374341b3
samreid <reids@colorado.edu>
2020-01-21 12:32:22 -0700

    Use standard form for require statements, see https://github.com/phetsims/chipper/issues/820

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 // Copyright 2016, University of Colorado Boulder
 
-define( [
-  'PHETCOMMON/model/Bucket',
-  'PHETCOMMON/model/Fraction',
-  'PHETCOMMON/model/SphereBucket',
-  'PHETCOMMON/util/StringUtils',
-  'PHETCOMMON/view/ModelViewTransform2'
-], function( phetcommon ) {
+define( require => {
+
   'use strict';
+
+  const phetcommon = require( 'PHETCOMMON/phetcommon' );
+
+  require( 'PHETCOMMON/model/Bucket' );
+  require( 'PHETCOMMON/model/Fraction' );
+  require( 'PHETCOMMON/model/SphereBucket' );
+  require( 'PHETCOMMON/util/StringUtils' );
+  require( 'PHETCOMMON/view/ModelViewTransform2' );
   return phetcommon;
 } );
