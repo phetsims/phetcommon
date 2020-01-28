4581402ada21a7a0c7fdb6d5feb1edb499e98ecd
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 19:23:06 -0900

    move stripEmbeddingMarks to phet-core, use in UtteranceQueue and AccessibilityUtils, https://github.com/phetsims/chipper/issues/798

--- js/util/StringUtils.js
+++ js/util/StringUtils.js
@@ -276,17 +276,6 @@ define( require => {
     },
 
     /**
-     * Strips embedding marks out of a string.
-     * @public
-     *
-     * @param {string} string
-     * @returns {string}
-     */
-    stripEmbeddingMarks: function( string ) {
-      return string.replace( /\u202a|\u202b|\u202c/g, '' );
-    },
-
-    /**
      * Replaces embedding mark characters with visible strings. Useful for debugging for strings with embedding marks.
      * @public
      *
