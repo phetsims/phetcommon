d8ce6b50b864d79819f0f2e8eb6c9469e2b6815e
phet-dev <phetadmin@gmail.com>
2020-02-28 03:42:31 -0700

    update copyright dates from daily grunt work

--- js/view/ModelViewTransform2.js
+++ js/view/ModelViewTransform2.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2019, University of Colorado Boulder
+// Copyright 2013-2020, University of Colorado Boulder
 
 /**
  * Transform between model and view coordinate frames, and provides convenience methods beyond dot.Transform3
