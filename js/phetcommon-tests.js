22b075c572b2f49c69e85d4349933d3f70c18105
samreid <reids@colorado.edu>
2020-05-21 14:54:15 -0600

    Use qunitStart, see https://github.com/phetsims/aqua/issues/89

--- js/phetcommon-tests.js
+++ js/phetcommon-tests.js
@@ -8,6 +8,7 @@
 
 import './model/FractionTests.js';
 import './util/StringUtilsTests.js';
+import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
 
 // Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
-QUnit.start();
+qunitStart();
