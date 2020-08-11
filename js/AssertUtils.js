219060b55fd4c38dc5526173167930bcf62212ff
pixelzoom <cmalley@pixelzoom.com>
2020-08-11 13:26:34 -0600

    include value in assertion message for assertInteger

--- js/AssertUtils.js
+++ js/AssertUtils.js
@@ -55,7 +55,7 @@ const AssertUtils = {
    * @public
    */
   assertInteger( value, predicate ) {
-    assert && assert( typeof value === 'number' && Utils.isInteger( value ), 'invalid value' );
+    assert && assert( typeof value === 'number' && Utils.isInteger( value ), `invalid value: ${value}` );
     if ( predicate ) {
       assert && assert( predicate( value ), `value does not satisfy predicate: ${value}` );
     }
