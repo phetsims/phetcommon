0d46d163adbbb7ee2441edc5547e223b9c3d01b5
samreid <reids@colorado.edu>
2020-09-29 14:15:32 -0600

    Move Matrix3IO to Matrix3.js, see https://github.com/phetsims/tandem/issues/212

--- js/view/ModelViewTransform2IO.js
+++ js/view/ModelViewTransform2IO.js
@@ -6,7 +6,7 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import Matrix3IO from '../../../dot/js/Matrix3IO.js';
+import Matrix3 from '../../../dot/js/Matrix3.js';
 import IOType from '../../../tandem/js/types/IOType.js';
 import phetcommon from '../phetcommon.js';
 import ModelViewTransform2 from './ModelViewTransform2.js';
@@ -14,8 +14,8 @@ import ModelViewTransform2 from './ModelViewTransform2.js';
 const ModelViewTransform2IO = new IOType( 'ModelViewTransform2IO', {
   valueType: ModelViewTransform2,
   documentation: 'Object type that supports 2 dimensional coordinate transforms.',
-  toStateObject: modelViewTransform2 => ( { matrix: Matrix3IO.toStateObject( modelViewTransform2.matrix ) } ),
-  fromStateObject: stateObject => new ModelViewTransform2( Matrix3IO.fromStateObject( stateObject.matrix ) )
+  toStateObject: modelViewTransform2 => ( { matrix: Matrix3.Matrix3IO.toStateObject( modelViewTransform2.matrix ) } ),
+  fromStateObject: stateObject => new ModelViewTransform2( Matrix3.Matrix3IO.fromStateObject( stateObject.matrix ) )
 } );
 
 phetcommon.register( 'ModelViewTransform2IO', ModelViewTransform2IO );
