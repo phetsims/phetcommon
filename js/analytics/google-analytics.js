a3320f3d2279896747c122919550d8a2fd80191e
samreid <reids@colorado.edu>
2021-06-29 12:34:51 -0600

    Enable lint rule no-sequences and ignore existing occurrences, see https://github.com/phetsims/chipper/issues/814

--- js/analytics/google-analytics.js
+++ js/analytics/google-analytics.js
@@ -104,8 +104,8 @@
       i.GoogleAnalyticsObject = r;
       i[ r ] = i[ r ] || function() {
         ( i[ r ].q = i[ r ].q || [] ).push( arguments );
-      }, i[ r ].l = 1 * new Date();
-      a = s.createElement( o ), m = s.getElementsByTagName( o )[ 0 ];
+      }, i[ r ].l = 1 * new Date(); // eslint-disable-line no-sequences
+      a = s.createElement( o ), m = s.getElementsByTagName( o )[ 0 ]; // eslint-disable-line no-sequences
       a.async = 1;
       a.src = g;
       m.parentNode.insertBefore( a, m );
