61b115f91255194f380625398b60c0f0fe55afd9
phet-dev <phetadmin@gmail.com>
2022-01-07 03:26:04 -0700

    update copyright dates from daily grunt work

--- js/AssertUtils.js
+++ js/AssertUtils.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * AssertUtils is a collection of utility functions for common assertions. Many of these assertions are related to
