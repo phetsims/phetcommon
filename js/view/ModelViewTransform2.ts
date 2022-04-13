03a6081063e82ddcd27ca4e38285fcad6a708777
pixelzoom <cmalley@pixelzoom.com>
2022-04-12 18:47:35 -0600

    convert ModelViewTransform to TS

--- js/view/ModelViewTransform2.ts
+++ js/view/ModelViewTransform2.ts
@@ -1,6 +1,5 @@
 // Copyright 2013-2020, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Transform between model and view coordinate frames, and provides convenience methods beyond dot.Transform3
  *
@@ -12,106 +11,132 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import Bounds2 from '../../../dot/js/Bounds2.js';
 import Matrix3 from '../../../dot/js/Matrix3.js';
+import Ray2 from '../../../dot/js/Ray2.js';
 import Transform3 from '../../../dot/js/Transform3.js';
 import Vector2 from '../../../dot/js/Vector2.js';
+import { Shape } from '../../../kite/js/imports.js';
 import phetcommon from '../phetcommon.js';
 
 class ModelViewTransform2 extends Transform3 {
 
-  // @public convenience model => view
-  modelToViewPosition( point ) { return this.transformPosition2( point ); }
+  //-------------------------------------------------------------------------------------------------------------
+  //  convenience model => view
+  //-------------------------------------------------------------------------------------------------------------
 
-  // @public
-  modelToViewXY( x, y ) { return new Vector2( this.modelToViewX( x ), this.modelToViewY( y ) ); }
+  public modelToViewPosition( point: Vector2 ): Vector2 {
+    return this.transformPosition2( point );
+  }
 
-  // @public
-  modelToViewX( x ) { return this.matrix.m00() * x + this.matrix.m02();}
+  public modelToViewXY( x: number, y: number ): Vector2 {
+    return new Vector2( this.modelToViewX( x ), this.modelToViewY( y ) );
+  }
 
-  // @public
-  modelToViewY( y ) { return this.matrix.m11() * y + this.matrix.m12();}
+  public modelToViewX( x: number ): number {
+    return this.matrix.m00() * x + this.matrix.m02();
+  }
 
-  // @public
-  modelToViewDelta( vector ) { return this.transformDelta2( vector ); }
+  public modelToViewY( y: number ): number {
+    return this.matrix.m11() * y + this.matrix.m12();
+  }
 
-  // @public
-  modelToViewNormal( normal ) { return this.transformNormal2( normal ); }
+  public modelToViewDelta( vector: Vector2 ): Vector2 {
+    return this.transformDelta2( vector );
+  }
 
-  // @public
-  modelToViewDeltaX( x ) { return this.transformDeltaX( x ); }
+  public modelToViewNormal( normal: Vector2 ): Vector2 {
+    return this.transformNormal2( normal );
+  }
 
-  // @public
-  modelToViewDeltaY( y ) { return this.transformDeltaY( y ); }
+  public modelToViewDeltaX( x: number ): number {
+    return this.transformDeltaX( x );
+  }
 
-  // @public
-  modelToViewBounds( bounds ) { return this.transformBounds2( bounds ); }
+  public modelToViewDeltaY( y: number ): number {
+    return this.transformDeltaY( y );
+  }
 
-  // @public
-  modelToViewShape( shape ) { return this.transformShape( shape ); }
+  public modelToViewBounds( bounds: Bounds2 ): Bounds2 {
+    return this.transformBounds2( bounds );
+  }
 
-  // @public
-  modelToViewRay( ray ) { return this.transformRay2( ray ); }
+  public modelToViewShape( shape: Shape ): Shape {
+    return this.transformShape( shape );
+  }
 
-  // @public convenience view => model
-  viewToModelPosition( point ) { return this.inversePosition2( point ); }
+  public modelToViewRay( ray: Ray2 ): Ray2 {
+    return this.transformRay2( ray );
+  }
 
-  // @public
-  viewToModelXY( x, y ) { return new Vector2( this.viewToModelX( x ), this.viewToModelY( y ) ); }
+  //-------------------------------------------------------------------------------------------------------------
+  //  convenience view => model
+  //-------------------------------------------------------------------------------------------------------------
 
-  // @public
-  viewToModelX( x ) {
+  public viewToModelPosition( point: Vector2 ): Vector2 {
+    return this.inversePosition2( point );
+  }
+
+  public viewToModelXY( x: number, y: number ): Vector2 {
+    return new Vector2( this.viewToModelX( x ), this.viewToModelY( y ) );
+  }
+
+  public viewToModelX( x: number ): number {
     const inverse = this.getInverse();
     return inverse.m00() * x + inverse.m02();
   }
 
-  // @public
-  viewToModelY( y ) {
+  public viewToModelY( y: number ): number {
     const inverse = this.getInverse();
     return inverse.m11() * y + inverse.m12();
   }
 
-  // @public
-  viewToModelDelta( vector ) { return this.inverseDelta2( vector ); }
-
-  // @public
-  viewToModelDeltaXY( x, y ) { return new Vector2( this.viewToModelDeltaX( x ), this.viewToModelDeltaY( y ) ); }
-
-  // @public
-  viewToModelNormal( normal ) { return this.inverseNormal2( normal ); }
-
-  // @public
-  viewToModelDeltaX( x ) { return this.inverseDeltaX( x ); }
-
-  // @public
-  viewToModelDeltaY( y ) { return this.inverseDeltaY( y ); }
-
-  // @public
-  viewToModelBounds( bounds ) { return this.inverseBounds2( bounds ); }
-
-  // @public
-  viewToModelShape( shape ) { return this.inverseShape( shape ); }
-
-  // @public
-  viewToModelRay( ray ) { return this.inverseRay2( ray ); }
-
-  // @public @overrides ModelViewTransform2 does not support arbitrary rotations.
-  validateMatrix( matrix ) {
-    super.validateMatrix( matrix );
-    assert && assert( matrix.isAligned(), 'matrix must be aligned' );
+  public viewToModelDelta( vector: Vector2 ): Vector2 {
+    return this.inverseDelta2( vector );
   }
 
-  /*---------------------------------------------------------------------------*
-   * Mutators.  Like its parent class, ModelViewTransform2 is mutable, and
-   * sends out notifications when changed.
-   *----------------------------------------------------------------------------*/
+  public viewToModelDeltaXY( x: number, y: number ): Vector2 {
+    return new Vector2( this.viewToModelDeltaX( x ), this.viewToModelDeltaY( y ) );
+  }
+
+  public viewToModelNormal( normal: Vector2 ): Vector2 {
+    return this.inverseNormal2( normal );
+  }
+
+  public viewToModelDeltaX( x: number ): number {
+    return this.inverseDeltaX( x );
+  }
+
+  public viewToModelDeltaY( y: number ): number {
+    return this.inverseDeltaY( y );
+  }
+
+  public viewToModelBounds( bounds: Bounds2 ): Bounds2 {
+    return this.inverseBounds2( bounds );
+  }
+
+  public viewToModelShape( shape: Shape ): Shape {
+    return this.inverseShape( shape );
+  }
+
+  public viewToModelRay( ray: Ray2 ) {
+    return this.inverseRay2( ray );
+  }
+
+
+  protected override validateMatrix( matrix: Matrix3 ): void {
+    super.validateMatrix( matrix );
+    assert && assert( matrix.isAligned(), 'matrix must be aligned, ModelViewTransform2 does not support arbitrary rotations' );
+  }
+
+  //-------------------------------------------------------------------------------------------------------------
+  // Mutators.  Like its parent class, ModelViewTransform2 is mutable, and sends out notifications when changed.
+  //-------------------------------------------------------------------------------------------------------------
 
   /**
    * See ModelViewTransform2.createRectangleMapping
-   * @param {Bounds2} modelBounds
-   * @param {Bounds2} viewBounds
-   * @public
    */
-  setToRectangleMapping( modelBounds, viewBounds ) {
+  public setToRectangleMapping( modelBounds: Bounds2, viewBounds: Bounds2 ): this {
     const m00 = viewBounds.width / modelBounds.width;
     const m02 = viewBounds.x - m00 * modelBounds.x;
     const m11 = viewBounds.height / modelBounds.height;
@@ -122,11 +147,8 @@ class ModelViewTransform2 extends Transform3 {
 
   /**
    * See ModelViewTransform2.createRectangleInvertedYMapping
-   * @param {Bounds2} modelBounds
-   * @param {Bounds2} viewBounds
-   * @public
    */
-  setToRectangleInvertedYMapping( modelBounds, viewBounds ) {
+  public setToRectangleInvertedYMapping( modelBounds: Bounds2, viewBounds: Bounds2 ): this {
     const m00 = viewBounds.width / modelBounds.width;
     const m02 = viewBounds.x - m00 * modelBounds.x;
     const m11 = -viewBounds.height / modelBounds.height;
@@ -143,10 +165,8 @@ class ModelViewTransform2 extends Transform3 {
 
   /**
    * Creates a ModelViewTransform2 that uses the identity transform (i.e. model coordinates are the same as view coordinates)
-   * @returns {ModelViewTransform2}
-   * @public
    */
-  static createIdentity() {
+  public static createIdentity(): ModelViewTransform2 {
     return new ModelViewTransform2( Matrix3.IDENTITY );
   }
 
@@ -154,12 +174,10 @@ class ModelViewTransform2 extends Transform3 {
    * Creates a ModelViewTransform2 that has the specified scale and offset such that
    * view = model * scale + offset
    *
-   * @param offset {Vector2} the offset in view coordinates
-   * @param scale  {number} the scale to map model to view
-   * @returns {ModelViewTransform2}
-   * @public
+   * @param offset - the offset in view coordinates
+   * @param scale - the scale to map model to view
    */
-  static createOffsetScaleMapping( offset, scale ) {
+  public static createOffsetScaleMapping( offset: Vector2, scale: number ): ModelViewTransform2 {
     return new ModelViewTransform2( Matrix3.affine( scale, 0, offset.x, 0, scale, offset.y ) );
   }
 
@@ -168,27 +186,25 @@ class ModelViewTransform2 extends Transform3 {
    * view.x = model.x * xScale + offset.x
    * view.y = model.y * yScale + offset.y
    *
-   * @param offset {Vector2} the offset in view coordinates
-   * @param xScale {number} the scale to map model to view in the x-dimension
-   * @param yScale {number} the scale to map model to view in the y-dimension
-   * @returns {ModelViewTransform2}
-   * @public
+   * @param offset - the offset in view coordinates
+   * @param xScale - the scale to map model to view in the x-dimension
+   * @param yScale - the scale to map model to view in the y-dimension
    */
-  static createOffsetXYScaleMapping( offset, xScale, yScale ) {
+  public static createOffsetXYScaleMapping( offset: Vector2, xScale: number, yScale: number ): ModelViewTransform2 {
     return new ModelViewTransform2( Matrix3.affine( xScale, 0, offset.x, 0, yScale, offset.y ) );
   }
 
   /**
-   * Creates a shearless ModelViewTransform2 that maps the specified model point to the specified view point, with the given x and y scales.
+   * Creates a shearless ModelViewTransform2 that maps the specified model point to the specified view point,
+   * with the given x and y scales.
    *
-   * @param modelPoint {Vector2} the reference point in the model which maps to the specified view point
-   * @param viewPoint  {Vector2} the reference point in the view
-   * @param xScale     {number} the amount to scale in the x direction
-   * @param yScale     {number} the amount to scale in the y direction
-   * @returns {ModelViewTransform2}
-   * @public
+   * @param modelPoint - the reference point in the model which maps to the specified view point
+   * @param viewPoint - the reference point in the view
+   * @param xScale - the amount to scale in the x direction
+   * @param yScale - the amount to scale in the y direction
    */
-  static createSinglePointXYScaleMapping( modelPoint, viewPoint, xScale, yScale ) {
+  public static createSinglePointXYScaleMapping(
+    modelPoint: Vector2, viewPoint: Vector2, xScale: number, yScale: number ): ModelViewTransform2 {
 
     // mx * scale + ox = vx
     // my * scale + oy = vy
@@ -198,59 +214,57 @@ class ModelViewTransform2 extends Transform3 {
   }
 
   /**
-   * Creates a shearless ModelViewTransform2 that maps the specified model point to the specified view point, with the given scale factor for both x and y dimensions.
+   * Creates a shearless ModelViewTransform2 that maps the specified model point to the specified view point,
+   * with the given scale factor for both x and y dimensions.
    *
-   * @param modelPoint {Vector2} the reference point in the model which maps to the specified view point
-   * @param viewPoint  {Vector2} the reference point in the view
-   * @param scale      {number} the amount to scale in the x and y directions
-   * @returns {ModelViewTransform2}
-   * @public
+   * @param modelPoint - the reference point in the model which maps to the specified view point
+   * @param viewPoint - the reference point in the view
+   * @param scale - the amount to scale in the x and y directions
    */
-  static createSinglePointScaleMapping( modelPoint, viewPoint, scale ) {
+  public static createSinglePointScaleMapping(
+    modelPoint: Vector2, viewPoint: Vector2, scale: number ): ModelViewTransform2 {
     return ModelViewTransform2.createSinglePointXYScaleMapping( modelPoint, viewPoint, scale, scale );
   }
 
   /**
-   * Creates a shearless ModelViewTransform2 that maps the specified model point to the specified view point, with the given scale factor for both x and y dimensions,
-   * but inverting the y axis so that +y in the model corresponds to -y in the view.
-   * Inverting the y axis is commonly necessary since +y is usually up in textbooks and -y is down in pixel coordinates.
+   * Creates a shearless ModelViewTransform2 that maps the specified model point to the specified view point,
+   * with the given scale factor for both x and y dimensions, but inverting the y axis so that +y in the model
+   * corresponds to -y in the view. Inverting the y axis is commonly necessary since +y is usually up in textbooks
+   * and -y is down in pixel coordinates.
    *
-   * @param modelPoint {Vector2} the reference point in the model which maps to the specified view point
-   * @param viewPoint  {Vector2} the reference point in the view
-   * @param scale      {number} the amount to scale in the x and y directions
-   * @returns {ModelViewTransform2}
-   * @public
+   * @param modelPoint - the reference point in the model which maps to the specified view point
+   * @param viewPoint - the reference point in the view
+   * @param scale - the amount to scale in the x and y directions
    */
-  static createSinglePointScaleInvertedYMapping( modelPoint, viewPoint, scale ) {
+  public static createSinglePointScaleInvertedYMapping(
+    modelPoint: Vector2, viewPoint: Vector2, scale: number ): ModelViewTransform2 {
     return ModelViewTransform2.createSinglePointXYScaleMapping( modelPoint, viewPoint, scale, -scale );
   }
 
   /**
-   * Creates a shearless ModelViewTransform2 that maps the specified rectangle in the model to the specified rectangle in the view,
-   * so that any point x% of the way across and y% down in the model rectangle will be mapped to the corresponding point x% across and y% down in the view rectangle.
-   * Linear extrapolation is performed outside of the rectangle bounds.
+   * Creates a shearless ModelViewTransform2 that maps the specified rectangle in the model to the specified rectangle
+   * in the view, so that any point x% of the way across and y% down in the model rectangle will be mapped to the
+   * corresponding point x% across and y% down in the view rectangle. Linear extrapolation is performed outside of
+   * the rectangle bounds.
    *
-   * @param modelBounds {Bounds2} the reference rectangle in the model, must have area > 0
-   * @param viewBounds  {Bounds2} the reference rectangle in the view, must have area > 0
-   * @returns {ModelViewTransform2}
-   * @public
+   * @param modelBounds - the reference rectangle in the model, must have area > 0
+   * @param viewBounds - the reference rectangle in the view, must have area > 0
    */
-  static createRectangleMapping( modelBounds, viewBounds ) {
+  public static createRectangleMapping( modelBounds: Bounds2, viewBounds: Bounds2 ): ModelViewTransform2 {
     return new ModelViewTransform2().setToRectangleMapping( modelBounds, viewBounds );
   }
 
   /**
-   * Creates a shearless ModelViewTransform2 that maps the specified rectangle in the model to the specified rectangle in the view,
-   * so that any point x% of the way across and y% down in the model rectangle will be mapped to the corresponding point x% across and (100-y)% down in the view rectangle.
-   * Linear extrapolation is performed outside of the rectangle bounds.
-   * Inverting the y axis is commonly necessary since +y is usually up in textbooks and -y is down in pixel coordinates.
+   * Creates a shearless ModelViewTransform2 that maps the specified rectangle in the model to the specified rectangle
+   * in the view, so that any point x% of the way across and y% down in the model rectangle will be mapped to the
+   * corresponding point x% across and (100-y)% down in the view rectangle. Linear extrapolation is performed outside
+   * of the rectangle bounds. Inverting the y axis is commonly necessary since +y is usually up in textbooks and -y
+   * is down in pixel coordinates.
    *
-   * @param modelBounds {Bounds2} the reference rectangle in the model, must have area > 0
-   * @param viewBounds  {Bounds2} the reference rectangle in the view, must have area > 0
-   * @returns {ModelViewTransform2}
-   * @public
+   * @param modelBounds - the reference rectangle in the model, must have area > 0
+   * @param viewBounds - the reference rectangle in the view, must have area > 0
    */
-  static createRectangleInvertedYMapping( modelBounds, viewBounds ) {
+  public static createRectangleInvertedYMapping( modelBounds: Bounds2, viewBounds: Bounds2 ): ModelViewTransform2 {
     return new ModelViewTransform2().setToRectangleInvertedYMapping( modelBounds, viewBounds );
   }
 }
