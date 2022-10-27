7a8a773306cbbed425c9b05635ba462e8473b899
Jonathan Olson <jonathan.olson@colorado.edu>
2022-10-27 10:45:07 -0600

    Updating phet-lib with main common code references.

---
+++ js/main.ts
@@ -0,0 +1,10 @@
+// Copyright 2022, University of Colorado Boulder
+
+import phetcommon from './phetcommon.js';
+import './model/Bucket.js';
+import './model/Fraction.js';
+import './model/SphereBucket.js';
+import './util/StringUtils.js';
+import './view/ModelViewTransform2.js';
+
+export default phetcommon;
