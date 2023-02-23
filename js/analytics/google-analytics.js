4fe65317f4bb9418234324567f2fa519cd9a1ce8
Jonathan Olson <jonathan.olson@colorado.edu>
2023-02-23 16:38:22 -0700

    Google Analytics EEA cookie prevention, while allowing cookies for other locations, see https://github.com/phetsims/website/issues/1190

--- js/analytics/google-analytics.js
+++ js/analytics/google-analytics.js
@@ -147,7 +147,13 @@
       gtag( 'js', new Date() );
       gtag( 'consent', 'default', {
         ad_storage: 'denied',
-        analytics_storage: 'denied'
+        analytics_storage: 'granted'
+      } );
+      // EEA analytics storage denied for cookies, see https://github.com/phetsims/website/issues/1190
+      gtag( 'consent', 'default', {
+        ad_storage: 'denied',
+        analytics_storage: 'denied',
+        region: [ 'BE', 'BG', 'CZ', 'DK', 'CY', 'LV', 'LT', 'LU', 'ES', 'FR', 'HR', 'IT', 'PL', 'PT', 'RO', 'SI', 'HU', 'MT', 'NL', 'AT', 'IS', 'LI', 'NO', 'SK', 'FI', 'SE', 'DE', 'EE', 'IE', 'EL' ]
       } );
       gtag( 'config', phet.chipper.queryParameters.ga4 );
 
@@ -169,7 +175,13 @@
     }
     gtmTag( 'consent', 'default', {
       ad_storage: 'denied',
-      analytics_storage: 'denied'
+      analytics_storage: 'granted'
+    } );
+    // EEA analytics storage denied for cookies, see https://github.com/phetsims/website/issues/1190
+    gtmTag( 'consent', 'default', {
+      ad_storage: 'denied',
+      analytics_storage: 'denied',
+      region: [ 'BE', 'BG', 'CZ', 'DK', 'CY', 'LV', 'LT', 'LU', 'ES', 'FR', 'HR', 'IT', 'PL', 'PT', 'RO', 'SI', 'HU', 'MT', 'NL', 'AT', 'IS', 'LI', 'NO', 'SK', 'FI', 'SE', 'DE', 'EE', 'IE', 'EL' ]
     } );
     window.dataLayer.push( {
       simBrand: phet.chipper.brand,
