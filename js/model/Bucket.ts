f7cb8e38c1a419971e24d0258ead50a4ec5cf45c
Sam Reid <reids@colorado.edu>
2023-04-12 13:47:44 -0600

    Convert to TS, see https://github.com/phetsims/phetcommon/issues/55

--- js/model/Bucket.ts
+++ js/model/Bucket.ts
@@ -17,22 +17,51 @@
 import Dimension2 from '../../../dot/js/Dimension2.js';
 import Vector2 from '../../../dot/js/Vector2.js';
 import { Shape } from '../../../kite/js/imports.js';
-import merge from '../../../phet-core/js/merge.js';
-import PhetioObject from '../../../tandem/js/PhetioObject.js';
+import PhetioObject, { PhetioObjectOptions } from '../../../tandem/js/PhetioObject.js';
 import phetcommon from '../phetcommon.js';
+import optionize from '../../../phet-core/js/optionize.js';
+import { TColor } from '../../../scenery/js/imports.js';
 
 // Proportion of the total height which the ellipse that represents the hole occupies.  It is assumed that the width
 // of the hole is the same as the width specified at construction.
 const HOLE_ELLIPSE_HEIGHT_PROPORTION = 0.25;
 
+type SelfOptions = {
+  position?: Vector2;
+  size?: Dimension2;
+  baseColor?: TColor;
+  captionText?: string;
+  captionColor?: TColor;
+  invertY?: boolean;
+};
+type BucketOptions = PhetioObjectOptions & SelfOptions;
+
 class Bucket extends PhetioObject {
 
-  /**
-   * @param {Object} [options]
-   */
-  constructor( options ) {
+  // The position is defined to be where the center of the hole is.
+  public readonly position: Vector2;
 
-    options = merge( {
+  // Base color of the bucket.
+  public readonly baseColor: TColor | null;
+
+  // Caption to be shown on the bucket.
+  public readonly captionText: string;
+
+  // Color for the caption.
+  public readonly captionColor: TColor;
+
+  // The size of the bucket.
+  public readonly size: Dimension2;
+
+  // Create the shape of the bucket's hole.
+  public readonly holeShape: Shape;
+
+  // The shape of the front portion of the bucket.
+  public readonly containerShape: Shape;
+
+  public constructor( providedOptions?: BucketOptions ) {
+
+    const options = optionize<BucketOptions, SelfOptions, PhetioObjectOptions>()( {
       position: Vector2.ZERO,
       size: new Dimension2( 200, 50 ),
       baseColor: '#ff0000',
@@ -42,31 +71,21 @@ class Bucket extends PhetioObject {
       // The following flag controls whether the bucket shape should be essentially upside down.  This allows it to be
       // used in cases where the model uses the inverted-y scheme commonly associated with screen layouts.
       invertY: false
-    }, options );
+    }, providedOptions );
 
     super( options );
 
-    // @public (read-only) - The position is defined to be where the center of the hole is.
     this.position = options.position;
-
-    // @public (read-only) - Base color of the bucket.
     this.baseColor = options.baseColor;
-
-    // @public (read-only) - Caption to be shown on the bucket.
     this.captionText = options.captionText;
-
-    // @public (read-only) - Color for the caption.
     this.captionColor = options.captionColor;
-
-    // @public (read-only) - The {Dimension2} size of the bucket
     this.size = options.size;
     const size = this.size;
 
     const holeRadiusX = size.width / 2;
     const holeRadiusY = size.height * HOLE_ELLIPSE_HEIGHT_PROPORTION / 2;
 
-    // @public (read-only) - Create the shape of the bucket's hole.
-    this.holeShape = Shape.ellipse( 0, 0, holeRadiusX, holeRadiusY );
+    this.holeShape = Shape.ellipse( 0, 0, holeRadiusX, holeRadiusY, 0 );
 
     // Create the shape of the container.  This code is a bit 'tweaky', meaning that there are a lot of fractional
     // multipliers in here to try to achieve the desired pseudo-3D look.  The intent is that the 'tilt' of the bucket
@@ -74,7 +93,6 @@ class Bucket extends PhetioObject {
     const containerHeight = size.height * ( 1 - ( HOLE_ELLIPSE_HEIGHT_PROPORTION / 2 ) );
     const multiplier = options.invertY ? 1 : -1;
 
-    // @public (read-only) - The shape of the front portion of the bucket.
     this.containerShape = new Shape().moveTo( -size.width * 0.5, 0 )
       .lineTo( -size.width * 0.4, multiplier * containerHeight * 0.8 )
       .cubicCurveTo( -size.width * 0.3, multiplier * ( containerHeight * 0.8 + size.height * HOLE_ELLIPSE_HEIGHT_PROPORTION * 0.6 ),
