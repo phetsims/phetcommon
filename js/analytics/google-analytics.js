09b97c2029a331e7979a1912b8eab069333b2b12
Jonathan Olson <jonathan.olson@colorado.edu>
2024-03-18 13:38:29 -0600

    Re-enabling Google Analytics EEA cookie prevention, see https://github.com/phetsims/phetcommon/issues/64, https://github.com/phetsims/website/issues/1190

--- js/analytics/google-analytics.js
+++ js/analytics/google-analytics.js
@@ -98,6 +98,12 @@
         ad_storage: 'denied',
         analytics_storage: 'granted'
       } );
+      // EEA analytics storage denied for cookies, see https://github.com/phetsims/website/issues/1190
+      gtag( 'consent', 'default', {
+        ad_storage: 'denied',
+        analytics_storage: 'denied',
+        region: [ 'BE', 'BG', 'CZ', 'DK', 'CY', 'LV', 'LT', 'LU', 'ES', 'FR', 'HR', 'IT', 'PL', 'PT', 'RO', 'SI', 'HU', 'MT', 'NL', 'AT', 'IS', 'LI', 'NO', 'SK', 'FI', 'SE', 'DE', 'EE', 'IE', 'EL' ]
+      } );
       gtag( 'config', phet.chipper.getQueryParameter( 'ga4' ) );
 
       // Dynamically load the script
@@ -119,6 +125,12 @@
     gtmTag( 'consent', 'default', {
       ad_storage: 'denied',
       analytics_storage: 'granted'
+    } );
+    // EEA analytics storage denied for cookies, see https://github.com/phetsims/website/issues/1190
+    gtmTag( 'consent', 'default', {
+      ad_storage: 'denied',
+      analytics_storage: 'denied',
+      region: [ 'BE', 'BG', 'CZ', 'DK', 'CY', 'LV', 'LT', 'LU', 'ES', 'FR', 'HR', 'IT', 'PL', 'PT', 'RO', 'SI', 'HU', 'MT', 'NL', 'AT', 'IS', 'LI', 'NO', 'SK', 'FI', 'SE', 'DE', 'EE', 'IE', 'EL' ]
     } );
     window.dataLayer.push( {
       simBrand: phet.chipper.brand,
